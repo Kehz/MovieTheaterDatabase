c01d61f77c91447f55b52abe0944b906e6644518
adevin0 <adamsdw@email.sc.edu>
2020-03-30 18:54:32 -0400

    initial commit

---
+++ src/TheaterDriver.java
@@ -0,0 +1,15 @@
+/*
+ * This is the driver class for our theater project 
+ * WIP
+ * @Author Devin Adams, Logan Etheredge, Brandon Carter
+ * Trello Board - https://trello.com/b/FDiXCUV5/insertgoodnameheremoviedatabase
+ * Repository	- https://github.com/adevin0/MovieTheaterDatabase
+ */
+public class TheaterDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
