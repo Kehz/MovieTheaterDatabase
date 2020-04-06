9f011c4869f2d0721f20c97b1fcb27a7c2f7f57a
Logan Etheredge <loganetheredge@Logans-MacBook-Pro-3.local>
2020-04-05 22:00:00 -0400

    updated methods for ordering tickets/reserving seats

--- src/main/User.java
+++ src/main/User.java
@@ -1,5 +1,9 @@
 package main;
 import java.util.Scanner;
+import java.util.ArrayList;
+
+import JSONParsing.DataLists;
+import JSONParsing.DataWriter;
 
 public class User {
 	Scanner in = new Scanner(System.in);
@@ -81,41 +85,91 @@ public class User {
 	}
 	
 	public void orderTicket() {
-		viewTheaters();
-		viewShows();
-		reserveSeating();
-		checkout();
+		Theaters theaterPick=viewTheaters();//select theater
+		Show showPick=viewShows(theaterPick);//select show from ones playing at the theater
+		reserveSeating(showPick);//reserve seat at that show
+		checkout(showPick);//pay for the ticket
 	}
 	
-	public void viewTheaters() {
-		//need instance of TheaterInterface
+	public Theaters viewTheaters() {
+		Theaters chosen= new Theaters(email, age, email);
+		DataLists dataLists = DataLists.getInstance(); //Pulls instance of the data list class
 		TheaterInterface thing= new TheaterInterface();//just to make sure the logic is right
-		thing.displayTheaters();
+		ArrayList<Theaters> theaterLists = dataLists.getTheaters();
 		//select a theater
 		System.out.println("What theater would you like to select?");
+		thing.displayTheaters();
 		//type in selection
 		String selection;
 		selection=in.nextLine();
 		//check to see if there is a match in the arraylist, then set that to selection
+		for(Theaters theaters : theaterLists) {
+			if (theaters.getTitle() == selection) {
+				chosen=theaters;
+			}
+			else
+			{
+				//redo selection until a valid one is chosen
+			}
+		}
+		
 		//return selection
+		return chosen;
 	}
 	
-	public void viewShows() {
-		//need instance of TheaterInterface
-		TheaterInterface thing= new TheaterInterface();//just to make sure the logic is right
-		thing.displayMovies();
-		thing.displayPlays();
-		//select a show
-		System.out.println("What show would you like to select?");
-		//type in selection
-		String selection;
-		selection=in.nextLine();
-		//check to see if there is a match in the arraylists, then set that to selection
-		//return selection
+	public Show viewShows(Theaters temp) {
+		Show chosen=new Show(age, email, age, email, email, age, email, email, null, age);
+		TheaterInterface thing= new TheaterInterface();
+		DataLists dataLists = DataLists.getInstance(); //Pulls instance of the data list class
+		
+		System.out.println("Would you like to view our list of movies or plays? Enter '1' for movies and '2' for plays");
+		int choose;
+		choose=in.nextInt();
+		if(choose==1) {
+			ArrayList<Movie> movieList = dataLists.getMovie(); //Grabs the current list of users/movies/plays
+			//select a show
+			System.out.println("What movie would you like to select?");
+			thing.displayMovies();
+			//type in selection
+			String selection;
+			selection=in.nextLine();
+			//check to see if there is a match in the arraylists, then set that to selection
+			
+			for(Movie movies : movieList) {
+				if (movies.getTitle() == selection) {
+					chosen=movies;
+				}
+				else
+				{
+					//redo selection until a valid one is chosen
+				}
+			}
+			
+		}
+		else if(choose==2) {
+			ArrayList<Play> playList = dataLists.getPlays(); //Grabs the current list of users/movies/plays
+			//select a show
+			System.out.println("What play would you like to select?");
+			thing.displayPlays();
+			//type in selection
+			String selection;
+			selection=in.nextLine();
+			//check to see if there is a match in the arraylists, then set that to selection
+			for(Play plays : playList) {
+				if (plays.getTitle() == selection) {
+					chosen=plays;
+				}
+				else
+				{
+					//redo selection until a valid one is chosen
+				}
+			}
+		}
+		return chosen;
 	}
 	
-	public void reserveSeating() {
-		Show temp = new Show();//just a placeholder
+	public void reserveSeating(Show temp) {
+		
 		Boolean[][] seats=temp.getSeating();
 		System.out.println("What row would you like to select?");
 		//type in seat selection
@@ -126,14 +180,16 @@ public class User {
 		int column;
 		column=in.nextInt();
 		seats[row][column]=true;
-		temp.setSeating(seats);
+		temp.setSeating(seats);//update show seating
 	}
 	
-	public void checkout() {
-		Show temp = new Show();//just a placeholder
+	public void checkout(Show temp) {
 		double cost= temp.getPrice();
 		System.out.println("A ticket to this show will cost " + cost + " dollars");
 		//implement discount methods here
 		double payment= in.nextDouble();
+		if(payment==cost) {
+			System.out.println("Transaction completed, enjoy your movie!");
+		}
 	}
 }
